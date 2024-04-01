aaac525a819aaa832e97de60caa28b50c36fa973
Komal D <komal@jessicafisherflyjets.onmicrosoft.com>
2024-04-01 15:17:36 -0400

    Final Solution

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using ContextListApp.client;
+using ContactListApp.client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
